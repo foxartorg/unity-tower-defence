fc4798d7ce3bc21d1df210f1d4410d119bfd79d7
yehorkosenko <kosenko.egor.ivanovich@gmail.com>
2022-10-07 18:41:28 +0300

    start creating shoor tower

--- Assets/Scripts/Tower/Tower.cs
+++ Assets/Scripts/Tower/Tower.cs
@@ -2,9 +2,21 @@ using UnityEngine;
 
 namespace Tower {
 	public class Tower : MonoBehaviour {
+		private float _fireCountDown = 1f;
+		private const float FireRate = 0f;
 
+		private void Update() {
+			if (_fireCountDown <= 0f) {
+				Shoot();
+				_fireCountDown = 1f / FireRate;
+			}
 
+			_fireCountDown -= Time.deltaTime;
+		}
 
-
+		// ReSharper disable Unity.PerformanceAnalysis
+		private void Shoot() {
+			Debug.Log("Shoot!");
+		}
 	}
 }
