474513a4f6cd7bf5338d12b5efaf289edf68b854
Pyotr <wijjhkkl@gmail.com>
2022-10-07 21:10:55 +0300

    Spawn enemy

--- Assets/Scripts/Enemy/SpawnEnemy.cs
+++ Assets/Scripts/Enemy/SpawnEnemy.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 namespace Enemy {
     public class SpawnEnemy : MonoBehaviour {
         [SerializeField] private GameObject enemyPrefab;
-        private int
-
+        [SerializeField] private int number;
 
         private void Start() {
                 StartCoroutine(EnemySpawn());
         }
+
         private IEnumerator EnemySpawn() {
-            for (var i = 0; i < 5; i++) {
-                CreateEnemy();
+            for (var i = 0; i < number; i++) {
+                var transform1 = transform;
+                Instantiate(enemyPrefab,transform1.position,transform1.rotation,transform1);
                 yield return new WaitForSeconds(0.5f);
             }
-        }
-        private void CreateEnemy() {
-            Instantiate(enemyPrefab,transform.position,transform.rotation);
-
-
-
         }
     }
 }
