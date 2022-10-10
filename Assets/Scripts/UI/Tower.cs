e596ae1a84bec2a6aa355b95ebd191e3fa0e3ac5
yehorkosenko <kosenko.egor.ivanovich@gmail.com>
2022-10-10 13:57:30 +0300

    fixing bugs3

--- Assets/Scripts/UI/Tower.cs
+++ Assets/Scripts/UI/Tower.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 namespace UI {
 	public class Tower : MonoBehaviour {
 		[SerializeField] private GameObject bulletPrefab;
-		[SerializeField] private Transform firePoint;
+		private Transform _transform;
 		private float _fireRate = 1f;
+
+		private void Start() {
+			_transform = transform;
+		}
 
 		private void Update() {
 			Shoot();
@@ -12,7 +16,7 @@ namespace UI {
 
 		private void Shoot() {
 			if (_fireRate <= 0f) {
-				Instantiate(bulletPrefab, firePoint.position, firePoint.rotation, transform);
+				Instantiate(bulletPrefab, _transform.position, _transform.rotation, _transform);
 				_fireRate = 1f;
 			}
 
