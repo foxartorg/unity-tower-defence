9b2a9b92c0853fba66dee205bfe3211e48872081
Pyotr <wijjhkkl@gmail.com>
2022-10-12 13:19:58 +0300

    Destroy on reaching the endpoint

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -9,8 +9,18 @@ namespace Enemy {
 			_navMeshAgent = GetComponent<NavMeshAgent>();
 		}
 
+		private void Update() {
+			Destroy();
+		}
+
 		public void Go(Vector3 position) {
 			_navMeshAgent.SetDestination(position);
+		}
+
+		private void Destroy() {
+			if (!_navMeshAgent.hasPath){
+				Destroy(gameObject);
+			}
 		}
 	}
 }
