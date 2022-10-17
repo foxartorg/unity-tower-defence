babffc155b7261073e298ec5714abaece09f348b
Ivan <foxartorg@gmail.com>
2022-10-18 02:27:55 +0300

    added enemy builder

--- Assets/App/Enemy/Enemy.cs
+++ Assets/App/Enemy/Enemy.cs
@@ -4,42 +4,38 @@ using UnityEngine.AI;
 using UnityEngine.UI;
 
 namespace App.Enemy {
-	public class Enemy : MonoBehaviour {
-		private NavMeshAgent _navMeshAgent;
-		private int _health = 100;
+	public class Enemy : EnemyBuilder {
+		private readonly int _health = 100;
 		private Slider _enemyHealthSlider;
 		private EnemyManager _enemyManager;
-
+		private NavMeshAgent _navMeshAgent;
 
 		private void Awake() {
 			this._navMeshAgent = this.GetComponent<NavMeshAgent>();
 			this._enemyHealthSlider = this.GetComponentInChildren<Slider>();
 			this._enemyManager = GameObject.Find("EnemyManager").GetComponent<EnemyManager>();
 			this._enemyHealthSlider.maxValue = this._health;
-
-
+			// this.OnDestroyEnemy += this.TestExtend;
+			// this.OnDestroyEnemy += () => Debug.Log(o.name);
 		}
 
 		private void Update() {
-			CanvasUI.EnemyHealthSlider(this._health,this._enemyHealthSlider);
-			this.DestroyEnemy();
-		}
-
-		private void EnemyDamage(int damage) {
-			this._health -= damage;
+			CanvasUI.EnemyHealthSlider(this._health, this._enemyHealthSlider);
+			this.CheckEnemy();
 		}
 
 		public void Go(Vector3 position) {
 			this._navMeshAgent.SetDestination(position);
 		}
 
-		private void DestroyEnemy() {
+		private void CheckEnemy() {
 			if (this._navMeshAgent.hasPath && this._health > 0) {
 				return;
 			}
 
-			this._enemyManager.counter -= 1;
-			this._enemyManager._main.canvasUI.EnemyCounterText(this._enemyManager.counter);
+			this.DestroyEnemy();
+			// this._enemyManager.counter -= 1;
+			// this._enemyManager._main.canvasUI.EnemyCounterText(this._enemyManager.counter);
 			Destroy(this.gameObject);
 		}
 	}
--- Assets/App/Enemy/EnemyBuilder.cs
+++ Assets/App/Enemy/EnemyBuilder.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
 namespace App.Enemy {
-	public class EnemyBuilder : MonoBehaviour { }
+	public class EnemyBuilder : MonoBehaviour {
+		public delegate void DestroyEnemyDelegate();
+
+		public event DestroyEnemyDelegate OnDestroyEnemy;
+
+		protected void DestroyEnemy() {
+			this.OnDestroyEnemy?.Invoke();
+		}
+	}
 }
