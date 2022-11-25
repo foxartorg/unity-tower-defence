4083b6233a1f022ab8debf48419b2dc711e795a1
yehorkosenko <kosenko.egor.ivanovich@gmail.com>
2022-11-25 22:21:30 +0200

    creating tower-upgrade

--- Assets/Scenes/GameScene/EventSystem.cs
+++ Assets/Scenes/GameScene/EventSystem.cs
@@ -1,4 +1,3 @@
-using System;
 using Common;
 using Src;
 using UnityEngine;
--- Assets/Scripts/Src/Tower/Tower.cs
+++ Assets/Scripts/Src/Tower/Tower.cs
@@ -3,23 +3,34 @@ using System.Linq;
 using Scenes.GameScene;
 using Src.Bullet;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Src.Tower {
 	public class Tower : MonoBehaviour {
 		private const float Timeout = 0.5f;
 		private readonly List<GameObject> _enemies;
-		private Transform _muzzleTransform;
+		private Button _button;
+		private Button _buttonSell;
+		private Button _buttonUpgrade;
 		private Transform _gunTransform;
+		private Transform _muzzleTransform;
 		private float _range;
+		private Renderer _renderer;
 		private float _timeout;
+		private GameObject canvas;
 		private Transform turret;
 
 		private Tower() {
 			this._enemies = new List<GameObject>();
 		}
- 
+
 		private void Awake() {
 			this.GetComponent<SphereCollider>().radius = this._range;
+			this._renderer = this.GetComponentInChildren<Renderer>();
+			this.canvas = GameObject.Find("TowerCanvas");
+			this._buttonUpgrade = this.transform.Find("TowerCanvas").Find("Buttons").Find("Upgrade").GetComponent<Button>();
+			this._buttonSell = this.transform.Find("TowerCanvas").Find("Buttons").Find("Sell").GetComponent<Button>();
+			this._button = this.transform.Find("TowerCanvas").Find("Buttons").Find("Hide").GetComponent<Button>();
 			this.turret = this.transform.Find("Head").Find("Turret");
 			this._gunTransform = this.transform.Find("Head").Find("Gun");
 			this._muzzleTransform = this.transform.Find("Head").Find("Muzzle");
@@ -27,13 +38,21 @@ namespace Src.Tower {
 			this._muzzleTransform.position = new Vector3(position.x, position.y, position.z - this._gunTransform.localScale.z * 3);
 		}
 
+		private void Start() {
+			this._button.onClick.AddListener(() => this.canvas.SetActive(false));
+			this._buttonSell.onClick.AddListener(() => TowerManager.Instance.DeleteTower(this.gameObject));
+			this._buttonUpgrade.onClick.AddListener(() => this.Upgrade());
+		}
+
 		private void OnDrawGizmos() {
 			Gizmos.color = Color.red;
 			Gizmos.DrawWireSphere(this.transform.position, this._range);
 		}
 
-		private void OnMouseEnter() {
-			Debug.Log("Tower OnMouseEnter");
+		private void OnMouseDown() {
+			if (Input.GetMouseButtonDown(0)) {
+				this.canvas.SetActive(true);
+			}
 		}
 
 		private void OnTriggerEnter(Collider other) {
@@ -44,7 +63,6 @@ namespace Src.Tower {
 			this._enemies.Add(other.gameObject);
 			other.gameObject.GetComponent<Enemy.Enemy>().enemies = this._enemies;
 			CanvasUI.Instance.TowerEnemyCount(this._enemies.Count);
-			// Debug.Log("ENEMY ENTERED!");
 		}
 
 		private void OnTriggerExit(Collider other) {
@@ -54,7 +72,6 @@ namespace Src.Tower {
 
 			this._enemies.Remove(other.gameObject);
 			CanvasUI.Instance.TowerEnemyCount(this._enemies.Count);
-			// Debug.Log("ENEMY EXIT!");
 		}
 
 		private void OnTriggerStay(Collider other) {
@@ -72,6 +89,11 @@ namespace Src.Tower {
 			}
 
 			this._timeout = Timeout;
+		}
+
+		private void Upgrade() {
+			this._range += 2f;
+			this._renderer.material.color = Color.green;
 		}
 
 		public void SetRange(int range) {
